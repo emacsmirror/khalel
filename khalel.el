f51941f933bb3778cf2cea2f6736969d8de3ae06
Hanno Perrey <hanno@hoowl.se>
2021-09-20 20:48:08 +0200

    bumps version

--- khalel.el
+++ khalel.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Hanno Perrey <hanno@hoowl.se>
 ;; Created: september 10, 2021
 ;; Modified: september 10, 2021
-;; Version: 0.1.3
+;; Version: 0.1.4
 ;; Keywords: event, calendar, ics, khal
 ;; Homepage: https://gitlab.com/hperrey/khalel
 ;; Package-Requires: ((emacs "27.1"))
